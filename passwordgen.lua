14834b4e7aca2553e6c8feabf870dc8593aaa616
MushroomGuy12323 <97201792+MushroomGuy12323@users.noreply.github.com>
2024-09-06 22:17:21 -0400

    Update passwordgen.lua via vscode.dev

--- passwordgen.lua
+++ passwordgen.lua
@@ -1,1 +1,20 @@
 local currentletter = ""
+
+print("enter amount of characters in password (or infinite)")
+io.write(">")
+local input = io.read()
+if input == "infinite" then
+    while true do
+        local currentletter = string.char(math.random(33,126))
+        io.write(currentletter)
+    end
+end
+local amnt = tonumber(input)
+if amnt ~= nil and amnt > 1 then
+    while amnt ~= 0 do
+        local currentletter = string.char(math.random(33,126))
+        io.write(currentletter)
+        amnt = amnt - 1
+    end
+    print(" ")
+end
