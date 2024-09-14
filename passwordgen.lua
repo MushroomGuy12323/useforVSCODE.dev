e1957fa13b76da4767e930052281a02fd3bc386a
MushroomGuy12323 <97201792+MushroomGuy12323@users.noreply.github.com>
2024-09-14 15:45:29 +0000

    skibidi

--- passwordgen.lua
+++ passwordgen.lua
@@ -5,4 +5,7 @@ local input = io.read()
 local ans = tonumber(input)
 if not ans == nil and ans >= 1 then
     while ans ~= 0 do
-        io.write(string.char(math.random(36,133)))
+        io.write(string.char(math.random(33,126)))
+        ans = ans - 1
+    end
+end
