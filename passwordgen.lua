be4f2daae25a3f47b6b56aa49d882be3fa918a1c
MushroomGuy12323 <97201792+MushroomGuy12323@users.noreply.github.com>
2024-09-14 15:47:56 +0000

    full password generetor

--- passwordgen.lua
+++ passwordgen.lua
@@ -3,9 +3,8 @@ print("pleae specify password length")
 io.write(">")
 local input = io.read()
 local ans = tonumber(input)
-if not ans == nil and ans >= 1 then
-    while ans ~= 0 do
-        io.write(string.char(math.random(33,126)))
-        ans = ans - 1
-    end
+while ans ~= 0 do
+    io.write(string.char(math.random(33,126)))
+    ans = ans - 1
 end
+print()
